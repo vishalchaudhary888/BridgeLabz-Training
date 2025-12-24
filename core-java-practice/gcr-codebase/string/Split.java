ebdead08e1bf5eea525a2e349bf78556d1918ff7
vishalchaudhary888 <vishalchaudhary5803@gmail.com>
2025-12-24 10:21:01 +0530

    [Vishal Chaudhary] Remove core-java-practice directory which was pushed mistakenly in the main branch instead of core-java-practice

--- core-java-practice/gcr-codebase/string/Split.java
+++
@@ -1,9 +0,0 @@
-public class Split {
-    public static char[] split(String str){
-        char[] Array = new char[Length.length(str)];
-        for(int i=0;i<Array.length;i++){
-            Array[i] = str.charAt(i);
-        }
-        return Array;
-    }
-}
