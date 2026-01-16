985b3ac3c832e7ce3a168fcb29f6d174b1bb3e54
Your Name <vishalchaudhary888>
2026-01-16 15:37:58 +0530

    [VishalChaudhary] Add . Roman to Integer leetcode question

---
+++ dsa-practice/leet-code-practice/RomanToInteger.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+class RomanSolution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+        mp.put('I', 1);
+        mp.put('V', 5);
+        mp.put('X', 10);
+        mp.put('L', 50);
+        mp.put('C', 100);
+        mp.put('D', 500);
+        mp.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = mp.get(s.charAt(i));
+            if (currentValue >= prevValue) {
+                result += currentValue;
+            } else {
+                result -= currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return result;
+    }
+}
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        RomanSolution rs = new RomanSolution();
+        System.out.println(rs.romanToInt("XIV"));
+    }
+}
